2eefe967f050a06080bbc2706c249f2e2de306dd
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-07 13:51:21 -0500

    ADD: validates emails

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,4 @@
 class Restaurant < ApplicationRecord
   validates :rating, :inclusion => 0..4
+  validates :email , uniqueness: { case_sensetive: false }, format:{ with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, multiline:true}
 end
