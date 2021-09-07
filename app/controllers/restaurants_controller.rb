b0ddd544b2b7e858370b84ae7f512d75fbfcd831
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-07 14:04:25 -0500

    ADD: crud operations

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,19 +1,45 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :update, :destroy]
 
   def index
+    render json: Restaurant.all
   end
 
   def show
+    render json: @restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      render json: @restaurant, status: :created
+    else
+      render json: @restaurant.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @restaurant.update(restaurant_params)
+      render json: @restaurant
+    else
+      render json: @bank_bink.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @restaurant.destroy
   end
 
- 
+  private
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :rating, :site, :email, :phone, :street, :city, :state, :lat, :lng)
+    end
 end
