aca863d2c2a4e6dfc8ca80fb3a891f3562a55a2c
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-07 22:45:27 -0500

    Fix: query

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,10 +3,9 @@ class Restaurant < ApplicationRecord
   validates :email , uniqueness: { case_sensetive: false }, format:{ with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, multiline:true}
 
   def self.search(longitude, latitude, radius)
-    query = "SELECT id, name, rating, geom  FROM restaurants WHERE ST_DWithin(geom, ST_MakePoint(#{longitude}, #{latitude})::geography, #{radius});"
-    data = ActiveRecord::Base.connection.execute(query)
+    data = Restaurant.where("ST_DWithin(geom, ST_MakePoint(#{longitude}, #{latitude})::geography, #{radius})")
     avg = data.inject(0.0) { |sum, e| sum + e["rating"] } / data.count
-    sum = data.sum(0.0) { |element| (element["rating"] - avg) ** 2 }
+    sum = data.map(&:rating).sum(0.0) { |element| (element - avg) ** 2 }
     variance = sum / (data.count - 1)
     standard_deviation = Math.sqrt(variance)
     {
