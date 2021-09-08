31022157b250f08a9e4462941555a5dbcaaca0da
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-08 13:22:22 -0500

    Fix: statics route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
-  get "restaurants/statics", to: "restaurants#statistics"
+  resources :restaurants do 
+    get "statistics", on: :collection
+  end
 end
